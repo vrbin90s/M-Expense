5ed53584b6126a69280568e0598c5920200f3db3
vrbin90s <72602872+vrbin90s@users.noreply.github.com>
2021-11-12 15:52:44 +0000

    NewExpensesActivity for Tomas to work on task C.

---
+++ app/src/main/java/uk/gre/ac/ks3319t/m_expense/AddTripExpenses.java
@@ -0,0 +1,14 @@
+package uk.gre.ac.ks3319t.m_expense;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddTripExpenses extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_trip_expenses);
+    }
+}
